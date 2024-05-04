e332a92b7430e169e5061c4da82c6182566ad69d
snehal <snehal@rasheedfoundation.org>
2024-05-04 22:26:28 +0530

    second commit

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express(); 
 const port = process.env.PORT || 3000; 
 // Serve static files from the Angular app 
-app.use(express.static(path.join(__dirname, 'angular-webpage'))); 
+app.use(express.static(path.join(__dirname, 'angular-webpage xxxxx'))); 
 // Define routes 
 app.get('/', (req, res) => { 
 res.sendFile(path.join(__dirname, './src/app/home/home.component.html')); 
