431830d9c98009c8098ef41681224ada2a6c8f08
snehal <snehal@rasheedfoundation.org>
2024-05-04 22:03:42 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+const path = require('path'); 
+const app = express(); 
+const port = process.env.PORT || 3000; 
+// Serve static files from the Angular app 
+app.use(express.static(path.join(__dirname, 'angular-webpage'))); 
+// Define routes 
+app.get('/', (req, res) => { 
+res.sendFile(path.join(__dirname, './src/app/home/home.component.html')); 
+}); 
+app.get('/about', (req, res) => { 
+res.sendFile(path.join(__dirname, './src/app/contact/contact.component.html')); 
+}); 
+app.get('/contact', (req, res) => { 
+res.sendFile(path.join(__dirname, './src/app/about/about.component.html')); 
+});
+// Start the server 
+app.listen(port, () => { 
+    console.log(`Server is running on http://localhost:${port}`); 
+    });
